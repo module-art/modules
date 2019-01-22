6e86f6023a2df8818ca8567b0bdd0ffa35e84d42
Sylvestre <sylve@batipos.fr>
2019-01-22 15:33:54 +0100

    special view

--- resources/views/common/type-contents-conditions.blade.php
+++ resources/views/common/type-contents-conditions.blade.php
@@ -3,9 +3,9 @@
     $type = $rubrique->inclusive_type;
   @endphp
   <div class="large-bloc type-contents" data-content_type='{{ $type['content_type'] }}' data-filtre="{{ $type['default_filtre'] }}" data-desc="{{ $type['descendant'] }}">
-    @if(View::exists(env('APP_THEME', 'module-art') . '.' . $context . '.type-list-'.$type->content_type))
+    @if(View::exists('themes.'.env('APP_THEME', 'module-art') . '.' . $context . '.type-list-'.$type->content_type))
       {{--next include redirect to the specific view if extists--}}
-      @include(env('APP_THEME', 'module-art') . '.' . $context . '.type-list-'.$type->content_type, [
+      @include('themes.'.env('APP_THEME', 'module-art') . '.' . $context . '.type-list-'.$type->content_type, [
         'results' => ModuleControl::getSortedTypeRubriques($type, $type->default_filtre, $type->descendant)
       ])
     @else
@@ -18,8 +18,8 @@
   </div>
 @elseif(isset($type_content))
   {{--$type_content is a rubrique--}}
-  @if(View::exists(env('APP_THEME', 'module-art') . '.' . $context . '.type-content-'.$type_content->type['content_type']))
-    @include(env('APP_THEME', 'module-art') . '.' . $context . '.type-content-'.$type_content->type['content_type'], [$type_content])
+  @if(View::exists('themes.'.env('APP_THEME', 'module-art') . '.' . $context . '.type-content-'.$type_content->type['content_type']))
+    @include('themes.'.env('APP_THEME', 'module-art') . '.' . $context . '.type-content-'.$type_content->type['content_type'], [$type_content])
   @else
     @include('common.' . $context . '.type-content', [$type_content])
   @endif
