f9814de255c122bfc4d5e32118aa3d3675fe3241
Sylvestre <sylve@batipos.fr>
2019-01-22 16:31:49 +0100

    mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,14 +13,14 @@ let mix = require('laravel-mix');
 
 var current_theme = process.env.APP_THEME ;
 
-mix.js('resources/assets/'+current_theme+'/js/scripts.js', 'public/js')
+mix.js('resources/views/themes/'+current_theme+'/assets/js/scripts.js', 'public/js')
    .js('resources/assets/common/js/admin.js', 'public/js')
    .js('resources/assets/common/js/contact.js', 'public/js')
    .js('resources/assets/common/js/insert_form.js', 'public/js')
    .js('resources/assets/common/js/categorie.js', 'public/js')
-   .sass('resources/assets/'+current_theme+'/sass/styles.scss', 'public/css')
-   .sass('resources/assets/'+current_theme+'/sass/admin.scss', 'public/css');
+   .sass('resources/views/themes/'+current_theme+'/assets/sass/styles.scss', 'public/css')
+   .sass('resources/views/themes/'+current_theme+'/assets/sass/admin.scss', 'public/css');
 
 if(current_theme == 'grands_chemins'){
-  mix.sass('resources/assets/'+current_theme+'/sass/tiny_custom.scss', 'public/css')
+  mix.sass('resources/views/themes/'+current_theme+'/assets/sass/tiny_custom.scss', 'public/css')
 }
