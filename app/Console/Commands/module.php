b09182d9d1e332a3aee2984cbf7e1613ade19031
Sylvestre <sylve@batipos.fr>
2019-06-11 09:52:00 +0200

    module:link command with choice

--- app/Console/Commands/module.php
+++ app/Console/Commands/module.php
@@ -11,7 +11,7 @@ class module extends Command
    *
    * @var string
    */
-  protected $signature = 'module:link {feature=views : all,controllers,requests...} {--T|theme= : required ! your theme\'s name}';
+  protected $signature = 'module:link {feature=views : all,controllers,requests...}';
 
   /**
    * The console command description.
@@ -43,62 +43,87 @@ class module extends Command
     if(!in_array(config('app.theme'), $themes)){
       $this->error('It seems that you didn\'t install your theme yet ?');
     }else{
-      //$feature = $this->choice('which feature must be linked ?', ['views (minimum)', 'Controllers', 'Requests', 'Repositories', 'all']);
+      $feature = $this->choice('Which feature must be linked (views is minimum)?', ['views', 'Controllers', 'Requests', 'Middleware', 'Repositories', 'all']);
 
-      //link views
-      $app_views_path = './resources/views/themes';
-      $theme_views_path = './Themes/' . config('app.theme') . '/views';
+      if(in_array($feature, ['all', 'views']))
+      {
+        //link views
+        $app_views_path = './resources/views/themes';
+        $theme_views_path = './Themes/' . config('app.theme') . '/views';
 
-      if(!file_exists($app_views_path)){
-        mkdir($app_views_path, 0755);
-      }
-      if(!file_exists($app_views_path . '/' . config('app.theme')) && file_exists($theme_views_path)){
-        symlink('../../.' . $theme_views_path, $app_views_path . '/' . config('app.theme'));
-      }
-
-      //link controllers
-      $app_controllers_path = './app/Http/Controllers/Themes';
-      $theme_controllers_path = './Themes/' . config('app.theme') . '/Http/Controllers';
-
-      if(!file_exists($app_controllers_path)){
-        mkdir($app_controllers_path, 0755);
-      }
-      if(!file_exists($theme_controllers_path)){
-        mkdir($theme_controllers_path, 0755, true);
-      }
-      if(!file_exists($app_controllers_path . '/' . config('app.theme'))){
-        symlink('../../../.' . $theme_controllers_path, $app_controllers_path . '/' . config('app.theme'));
+        if(!file_exists($app_views_path)){
+          mkdir($app_views_path, 0755);
+        }
+        if(!file_exists($app_views_path . '/' . config('app.theme')) && file_exists($theme_views_path)){
+          symlink('../../.' . $theme_views_path, $app_views_path . '/' . config('app.theme'));
+        }
       }
 
-      //link requests
-      $app_requests_path = './app/Http/Requests/Themes';
-      $theme_requests_path = './Themes/' . config('app.theme') . '/Http/Requests';
+      if(in_array($feature, ['all', 'Controllers'])){
+        //link controllers
+        $app_controllers_path = './app/Http/Controllers/Themes';
+        $theme_controllers_path = './Themes/' . config('app.theme') . '/Http/Controllers';
 
-      if(!file_exists($app_requests_path)){
-        mkdir($app_requests_path, 0755);
-      }
-      if(!file_exists($theme_requests_path)){
-        mkdir($theme_requests_path, 0755, true);
-      }
-      if(!file_exists($app_requests_path . '/' . config('app.theme'))){
-        symlink('../../../.' . $theme_requests_path, $app_requests_path . '/' . config('app.theme'));
-      }
-
-      //link repositories
-      $app_repositories_path = './app/Repositories/Themes';
-      $theme_repositories_path = './Themes/' . config('app.theme') . '/repositories';
-
-      if(!file_exists($app_repositories_path)){
-        mkdir($app_repositories_path, 0755);
-      }
-      if(!file_exists($theme_repositories_path)){
-        mkdir($theme_repositories_path, 0755);
-      }
-      if(!file_exists($app_repositories_path . '/' . config('app.theme'))){
-        symlink('../../.' . $theme_repositories_path, $app_repositories_path . '/' . config('app.theme'));
+        if(!file_exists($app_controllers_path)){
+          mkdir($app_controllers_path, 0755);
+        }
+        if(!file_exists($theme_controllers_path)){
+          mkdir($theme_controllers_path, 0755, true);
+        }
+        if(!file_exists($app_controllers_path . '/' . config('app.theme'))){
+          symlink('../../../.' . $theme_controllers_path, $app_controllers_path . '/' . config('app.theme'));
+        }
       }
 
-      $this->info('Your module theme is linked !');
+      if(in_array($feature, ['all', 'Requests'])){
+        //link requests
+        $app_requests_path = './app/Http/Requests/Themes';
+        $theme_requests_path = './Themes/' . config('app.theme') . '/Http/Requests';
+
+        if(!file_exists($app_requests_path)){
+          mkdir($app_requests_path, 0755);
+        }
+        if(!file_exists($theme_requests_path)){
+          mkdir($theme_requests_path, 0755, true);
+        }
+        if(!file_exists($app_requests_path . '/' . config('app.theme'))){
+          symlink('../../../.' . $theme_requests_path, $app_requests_path . '/' . config('app.theme'));
+        }
+      }
+
+      if(in_array($feature, ['all', 'Middleware'])){
+        //link middleware
+        $app_middleware_path = './app/Http/Middleware/Themes';
+        $theme_middleware_path = './Themes/' . config('app.theme') . '/Http/Middleware';
+
+        if(!file_exists($app_middleware_path)){
+          mkdir($app_middleware_path, 0755);
+        }
+        if(!file_exists($theme_middleware_path)){
+          mkdir($theme_middleware_path, 0755, true);
+        }
+        if(!file_exists($app_middleware_path . '/' . config('app.theme'))){
+          symlink('../../../.' . $theme_middleware_path, $app_middleware_path . '/' . config('app.theme'));
+        }
+      }
+
+      if(in_array($feature, ['all', 'Repositories'])){
+        //link repositories
+        $app_repositories_path = './app/Repositories/Themes';
+        $theme_repositories_path = './Themes/' . config('app.theme') . '/repositories';
+
+        if(!file_exists($app_repositories_path)){
+          mkdir($app_repositories_path, 0755);
+        }
+        if(!file_exists($theme_repositories_path)){
+          mkdir($theme_repositories_path, 0755);
+        }
+        if(!file_exists($app_repositories_path . '/' . config('app.theme'))){
+          symlink('../../.' . $theme_repositories_path, $app_repositories_path . '/' . config('app.theme'));
+        }
+      }
+
+      $this->info('Your module theme is linked for ' . $feature . ' !');
     }
 
   }
