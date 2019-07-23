5dab52be1dd25b669fab5163b541d02708e9924c
Sylvestre <sylve@batipos.fr>
2019-07-23 10:36:10 +0200

    run prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-var current_theme = process.env.APP_THEME;
+var current_theme = process.env.MODULES_THEME;
 mix.config.fileLoaderDirs.fonts = 'storage/'+current_theme+'/fonts';
 
 mix
