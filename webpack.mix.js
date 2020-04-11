9256436673f1af6e4fab8c3d4f9da6c39aef2657
Sylvestre <sylve@batipos.fr>
2020-04-11 22:30:24 +0200

    mix to themes 404 page

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,7 @@ var current_theme = process.env.MODULES_THEME;
 mix.config.fileLoaderDirs.fonts = 'storage/'+current_theme+'/fonts';
 
 mix
-   .js('Themes/'+current_theme+'/assets/js/scripts.js', 'public/js')
+   .js('Themes/'+current_theme+'/assets/js/scripts.js', 'public/js/themes/'+current_theme)
    //.js('resources/assets/js/app.js', 'public/js')
    //.js('resources/assets/js/admin.js', 'public/js')
    //.js('resources/assets/js/lists.js', 'public/js')
@@ -23,7 +23,7 @@ mix
    //.js('resources/assets/js/insert_form.js', 'public/js')
    //.js('resources/assets/js/categorie.js', 'public/js')
    //.js('resources/assets/js/fields.js', 'public/js')
-   .sass('Themes/'+current_theme+'/assets/sass/styles.scss', 'public/css')
+   .sass('Themes/'+current_theme+'/assets/sass/styles.scss', 'public/css/themes/'+current_theme)
    //.sass('Themes/'+current_theme+'/assets/sass/tiny_custom.scss', 'public/css')
    .sass('resources/assets/sass/admin.scss', 'public/css')
 ;
