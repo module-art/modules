f7e18e27f0e15c32468140efe1922ff2afaf88b6
module-art <contact@module-art.fr>
2023-04-11 15:52:11 +0200

    mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,7 +23,7 @@ mix.js('Themes/'+current_theme+'/assets/js/scripts.js', 'public/js/themes/'+curr
    //.js('resources/assets/js/app.js', 'public/js')
    //.js('resources/assets/js/admin.js', 'public/js')
    //.js('resources/assets/js/lists.js', 'public/js')
-   .js('resources/assets/js/contact.js', 'public/js')
+   //.js('resources/assets/js/contact.js', 'public/js')
    //.js('resources/assets/js/insert_form.js', 'public/js')
    //.js('resources/assets/js/categorie.js', 'public/js')
    //.js('resources/assets/js/fields.js', 'public/js')
